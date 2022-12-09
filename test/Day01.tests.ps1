3d339988d007c00aec2206c95db380eae4ee2cc4
Josh King <windosbk+github@gmail.com>
2022-12-09 22:34:52 +1300

    (fix) Syntax incorrect

--- test/Day01.tests.ps1
+++ test/Day01.tests.ps1
@@ -23,13 +23,13 @@ Describe "Day 1: Calorie Counting" {
     }
     Context "Part 1" {
         It "should return the correct answer" {
-            . (Join-Path -Path $DayPath -ChildPath 'Day01-1.ps1') PuzzleInput $PuzzleInput |
+            . (Join-Path -Path $DayPath -ChildPath 'Day01-1.ps1') -PuzzleInput $PuzzleInput |
                 Should -Be 24000
         }
     }
     Context "Part 2" {
         It "should return the correct answer" {
-            . (Join-Path -Path $DayPath -ChildPath 'Day01-2.ps1') PuzzleInput $PuzzleInput |
+            . (Join-Path -Path $DayPath -ChildPath 'Day01-2.ps1') -PuzzleInput $PuzzleInput |
                 Should -Be 45000
         }
     }
